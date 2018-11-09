405941197748c2c9fd8085fbe1a7867e31be25b1
Cam Prier <CSPrier@gmail.com>
2018-11-08 22:48:11 -0500

    Set up test suite but encountered an AssertionError that needs to be fixed

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ module.exports = {
 	CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
 	DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/programName',
 	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/programName',
+	MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/programName',
+	TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/programName-test',
 	JWT_SECRET: process.env.JWT_SECRET,
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
