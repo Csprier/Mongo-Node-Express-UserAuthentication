c4a7fac4b043dde4b7a9fe0137de218f91e5a8d2
Cam Prier <CSPrier@gmail.com>
2019-09-05 17:30:10 -0400

    Finializing security vulnerability

--- server.js
+++ server.js
@@ -57,8 +57,7 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-// Catch-all Error handler
-// Add NODE_ENV check to prevent stacktrace leak
+// Catch-all Error handler; Added NODE_ENV check to prevent stacktrace leak
 app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.json({
